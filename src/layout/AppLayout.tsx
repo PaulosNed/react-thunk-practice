8e967f2527f9183f3e0e6375d02714f2e5563060
unknown <pauldessie@gmail.com>
2023-08-14 15:59:15 +0300

    minor fix

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -6,7 +6,7 @@ export const AppLayout = () => {
     <div className="flex flex-col gap-10 bg-zinc-200 min-h-screen">
       <header className="w-full bg-black bg-opacity-95 pt-8 pb-2">
         <div className="w-full flex justify-between items-end px-12">
-          <p className="text-white text-3xl">Contact Manager</p>
+          <p className="text-white text-3xl">Note Taking application</p>
           <NavBar />
         </div>
       </header>
